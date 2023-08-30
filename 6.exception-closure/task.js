86e6248badcf7caeebe4cc0c64811a46a8e8a569
julia <y.andryushina@gmail.com>
2023-08-30 12:52:09 +0300

    Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,6 +1,7 @@
-﻿function parseCount(value) {
+﻿//1
+function parseCount(value) {
     const parsedValue = Number.parseFloat(value);
-    if (isNaN)(parsedValue) {
+    if (isNaN (parsedValue)) {
         throw new Error(`Невалидное значение`);
     }
     return parsedValue;
@@ -11,5 +12,39 @@ function validateCount(value) {
         return parseCount(value);
     } catch (error) {
         return error;
+    }
+}
+
+//2
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error(`Треугольник с такими сторонами не существует`);
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+    get area() {
+        const p = this.perimeter / 2;
+        return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
+      }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return `Ошибка! Треугольник не существует`;
+            },
+            get perimeter() {
+                return `Ошибка! Треугольник не существует`;
+            }
+        }
     }
 }
